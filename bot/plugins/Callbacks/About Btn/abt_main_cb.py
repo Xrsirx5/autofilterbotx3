f517ff53de11bdffd592b62def60160e8df7a1e1
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-09-28 03:35:48 +0530

    Update abt_main_cb.py

--- bot/plugins/Callbacks/About Btn/abt_main_cb.py
+++ bot/plugins/Callbacks/About Btn/abt_main_cb.py
@@ -39,7 +39,7 @@ async def cb_about(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/@A6rzuxpt69drdr6t9d5
                 ),
                 
             InlineKeyboardButton
